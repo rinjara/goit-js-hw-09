4048a9a5bb7959f78857f0e44bed973c1537406b
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-18 13:50:43 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const CHANGE_DELAY = 1000;
+let intervalId = null;
 
+const refs = {
+  startChangingColorBtn: document.querySelector('[data-start]'),
+  stopChangingColorBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.startChangingColorBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_DELAY);
+}
+
+function stopChangingBgColor() {
+  clearInterval(intervalId);
+  refs.startChangingColorBtn.disabled = false;
+}
+
+refs.startChangingColorBtn.addEventListener('click', changeBgColor);
+refs.stopChangingColorBtn.addEventListener('click', stopChangingBgColor);
